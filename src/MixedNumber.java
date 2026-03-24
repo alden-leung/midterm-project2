800aea3d35aaa28212fb01b93818c19b1e0dea6d
Ernst Cimatu <elic2262893@slu.edu.ph>
2026-03-24 20:33:12 +0800

    MixedNumber

---
+++ src/MixedNumber.java
@@ -0,0 +1,84 @@
+public class MixedNumber extends Fraction {
+
+    private int whole;
+
+    public MixedNumber() {
+        super();
+        this.whole = 0;
+    }
+
+    public MixedNumber(int whole, Fraction fraction) {
+        super(fraction.getNumerator(), fraction.getDenominator());
+        this.whole = whole;
+    }
+
+    public MixedNumber(int whole, int numerator, int denominator){
+        super(numerator, denominator);
+        this.whole = whole;
+    }
+
+    // convert improper fraction to mixed
+    public MixedNumber(Fraction fraction) {
+        int num = fraction.getNumerator();
+        int den = fraction.getDenominator();
+
+        this.whole = num / den;
+        setNumerator(num % den);
+        setDenominator(den);
+    }
+
+    public void setWholePart(int whole) {
+        this.whole = whole;
+    }
+
+    public void setFractionPart(Fraction fraction) {
+        setNumerator(fraction.getNumerator());
+        setDenominator(fraction.getDenominator());
+    }
+
+    public int getWhole() {
+        return whole;
+    }
+
+    public Fraction getFractionPart() {
+        return new Fraction(getNumerator(), getDenominator());
+    }
+
+    // REQUIRED BY UML
+    public Fraction toFraction() {
+        int num = whole * getDenominator() + getNumerator();
+        return new Fraction(num, getDenominator());
+    }
+
+    // REQUIRED OPERATIONS (MixedNumber param)
+    public MixedNumber add(MixedNumber other) {
+        Fraction result = this.toFraction().add(other.toFraction());
+        return new MixedNumber(result);
+    }
+
+    public MixedNumber subtract(MixedNumber other) {
+        Fraction result = this.toFraction().subtract(other.toFraction());
+        return new MixedNumber(result);
+    }
+
+    public MixedNumber multiplyBy(MixedNumber other) {
+        Fraction result = this.toFraction().multiplyBy(other.toFraction());
+        return new MixedNumber(result);
+    }
+
+    public MixedNumber divideBy(MixedNumber other) {
+        Fraction result = this.toFraction().divideBy(other.toFraction());
+        return new MixedNumber(result);
+    }
+
+    public String toString() {
+        if (getNumerator() == 0) {
+            return "" + whole;
+        }
+        return whole + " " + getNumerator() + "/" + getDenominator();
+    }
+
+    public double toDouble() {
+        return whole + super.toDouble();
+    }
+}
